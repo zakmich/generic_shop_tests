316851ba6faf758afc4acb644f0ade5191591aa9
Michał Żak <michal.zak93@gmail.com>
2024-01-30 21:12:00 +0100

    Initial commit of Generic shop login test

---
+++ tests/login.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("User login to Generic shop", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test("succesful login to shop", async ({ page }) => {
+    //Arange
+    const username = "fakeuser1@gmail.com";
+    const userPassword = "testowehaslo1234";
+
+    //Act
+    await page.getByRole("link", { name: " Account" }).click();
+    await page.getByLabel("Username or email address *").fill(username);
+    await page.locator("#password").fill(userPassword);
+    await page.getByRole("button", { name: "Login" }).click();
+
+    // Assert
+    await expect(page.locator('.page-title.margin-top')).toContainText(['My account']);
+  });
+});
