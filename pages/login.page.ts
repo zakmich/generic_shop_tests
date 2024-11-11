a0f729d60f58acae919b79ef6f0d842c04ae4ee7
Michał Żak <michal.zak93@gmail.com>
2024-11-11 22:53:46 +0100

    acces to class corected, loginButton selector corected

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,20 +1,28 @@
 import { Page } from "@playwright/test";
 
 export class LoginPage {
-    constructor(private page: Page) {}
-    
-    
-    accountLink = this.page.getByRole("link", { name: " Account" })
-    username = this.page.getByLabel("Username or email address *");
-    password = this.page.locator("#password");
-    loginButton = this.page.getByRole("button", { name: "Login" });
-    
-    assert = this.page.locator('//p[contains(text(),"Hello")]');
-    userNameError = this.page.locator('//div[@class="site-content"]//li[1]');
-    passwordError = this.page.locator('//li[contains(text(),"The password")]')
-    emptyUserNameField = this.page.locator('//div[@class="site-content"]//li[1]')
-    
-    async login(username: string, userPassword: string): Promise<void>{
+    private page: Page
+    private accountLink;
+    private username;
+    private password;
+    private loginButton;
+    assert;
+    userNameError;
+    passwordError;
+    emptyUserNameField;
+
+    constructor(page: Page) {
+        this.accountLink = this.page.getByRole("link", { name: " Account" });
+        this.username = this.page.getByLabel("Username or email address *");
+        this.password = this.page.locator("#password");
+        this.loginButton = this.page.getByRole("button", { name: "Log in" });
+        this.assert = this.page.locator('//p[contains(text(),"Hello")]');
+        this.userNameError = this.page.locator('//div[@class="site-content"]//li[1]');
+        this.passwordError = this.page.locator('//li[contains(text(),"The password")]');
+        this.emptyUserNameField = this.page.locator('//div[@class="site-content"]//li[1]');
+    }
+
+    async login(username: string, userPassword: string): Promise<void> {
         await this.accountLink.click();
         await this.username.fill(username);
         await this.password.fill(userPassword);
