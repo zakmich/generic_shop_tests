7ac5c9b2dd30166e5bc4002376e9aff897e89efb
Michał Żak <michal.zak93@gmail.com>
2024-11-17 23:30:40 +0100

    login.page updated due to new code checking rules -> variables initialized in constructor

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,31 +1,31 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class LoginPage {
-    private page: Page
-    private accountLink;
-    private username;
-    private password;
-    private loginButton;
-    assert;
-    userNameError;
-    passwordError;
-    emptyUserNameField;
-
-    constructor(page: Page) {
-        this.accountLink = this.page.getByRole("link", { name: " Account" });
+    accountLink: Locator;
+    username: Locator;
+    password: Locator;
+    loginButton: Locator;
+    
+    assert: Locator;
+    userNameError: Locator;
+    passwordError: Locator;
+    emptyUserNameField: Locator;
+    
+    constructor(private page: Page) {
+        this.accountLink = this.page.getByRole("link", { name: " Account" })
         this.username = this.page.getByLabel("Username or email address *");
         this.password = this.page.locator("#password");
         this.loginButton = this.page.getByRole("button", { name: "Log in" });
-        this.assert = this.page.locator('//p[contains(text(),"Hello")]');
-        this.userNameError = this.page.locator('//div[@class="site-content"]//li[1]');
-        this.passwordError = this.page.locator('//li[contains(text(),"The password")]');
-        this.emptyUserNameField = this.page.locator('//div[@class="site-content"]//li[1]');
+    
+        this.assert = this.page.getByText('fakeuser1');
+        this.userNameError = this.page.locator('//li[contains(text(),"Unknown email address. Check again or try your use")]');
+        this.passwordError = this.page.locator('//*[@id="post-8"]/div[2]/div[1]/ul/li/a')
+        this.emptyUserNameField = this.page.locator('//div[@class="site-content"]//li[1]')
     }
-
-    async login(username: string, userPassword: string): Promise<void> {
-        await this.accountLink.click();
-        await this.username.fill(username);
-        await this.password.fill(userPassword);
-        await this.loginButton.click();
+        async login(username: string, userPassword: string): Promise<void>{
+            await this.accountLink.click();
+            await this.username.fill(username);
+            await this.password.fill(userPassword);
+            await this.loginButton.click();
     }
 }
