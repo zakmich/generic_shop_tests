6404d9edce8f1e4bbcaa01c25f352603c09d9b23
Michał Żak <michal.zak93@gmail.com>
2024-11-17 23:31:35 +0100

    assertions updated

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,85 +8,86 @@ test.describe("User login to Generic shop", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
     loginpage = new LoginPage(page);
+
   });
 
   test("successful login to shop with correct credentials @L-TC01", async ({ page }) => {
-    // Arrange
+    //Arrange
     const username = loginData.username;
     const userPassword = loginData.userPassword;
     const loginMessage = "fakeuser1";
 
-    // Act
-    await loginpage.login(username, userPassword);
+    //Act
+    await loginpage.login(username, userPassword)
 
     // Assert
-    await expect(loginpage.assert).toContainText(loginMessage);
+    await expect(loginpage.assert).toContainText([loginMessage]);
   });
 
   test("unsuccessful login to shop with incorrect username and correct password @L-TC02", async ({ page }) => {
-    // Arrange
+    //Arrange
     const username = loginData.incorrectUsername;
     const userPassword = loginData.userPassword;
-    const userNameErrorMessage = "Unknown email address. Check again or try your username.";
+    const userNameErrorMessage = 'Unknown email address';
 
-    // Act
-    await loginpage.login(username, userPassword);
+    //Act
+    await loginpage.login(username, userPassword)
 
     // Assert
-    await expect(loginpage.userNameError).toContainText(userNameErrorMessage);
+    await expect(loginpage.userNameError).toContainText([userNameErrorMessage]);
   });
 
+  
   test("unsuccessful login to shop with correct username and incorrect password @L-TC03", async ({ page }) => {
-    // Arrange
+    //Arrange
     const username = loginData.username;
     const userPassword = loginData.incorrectPassword;
-    const passwordErrorMessage = "Error: The password you entered for the username fakeuser1@gmail.com is incorrect.";
+    const passwordErrorMessage = "Lost your password?";
 
-    // Act
-    await loginpage.login(username, userPassword);
+    //Act
+    await loginpage.login(username, userPassword)
 
     // Assert
-    await expect(loginpage.passwordError).toContainText(passwordErrorMessage);
+    await expect(loginpage.passwordError).toContainText([passwordErrorMessage]);
   });
 
   test("unsuccessful login to shop with empty username field and correct password @L-TC04", async ({ page }) => {
-    // Arrange
-    const username = "";
-    const userPassword = loginData.userPassword;
+    //Arrange
+    const username = ""
+    const userPassword = loginData.incorrectPassword;
     const emptyUsernameErrorMessage = "Error: Username is required.";
 
-    // Act
-    await loginpage.login(username, userPassword);
+    //Act
+    await loginpage.login(username, userPassword)
 
     // Assert
-    await expect(loginpage.userNameError).toContainText(emptyUsernameErrorMessage);
+    await expect(loginpage.emptyUserNameField).toContainText([emptyUsernameErrorMessage]);
   });
 
   test("unsuccessful login to shop with correct username and empty password field @L-TC05", async ({ page }) => {
-    // Arrange
-    const username = loginData.username;
-    const userPassword = "";
+    //Arrange
+    const username = loginData.username
+    const userPassword = ""
     const emptyPasswordMessage = "Error: The password field is empty.";
 
-    // Act
-    await loginpage.login(username, userPassword);
+    //Act
+    await loginpage.login(username, userPassword)
 
     // Assert
-    await expect(loginpage.passwordError).toContainText(emptyPasswordMessage);
+    await expect(loginpage.emptyUserNameField).toContainText([emptyPasswordMessage]);
   });
 
   test("unsuccessful login to shop with both fields empty @L-TC06", async ({ page }) => {
-    // Arrange
-    const username = "";
-    const userPassword = "";
+    //Arrange
+    const username = ""
+    const userPassword = ""
     const loginFieldsMessage = "Error: Username is required.";
 
-    // Act
-    await loginpage.login(username, userPassword);
+    //Act
+    await loginpage.login(username, userPassword)
 
     // Assert
-    await expect(loginpage.userNameError).toContainText(loginFieldsMessage);
+    await expect(loginpage.emptyUserNameField).toContainText([loginFieldsMessage]);
   });
-});
 
-"https://skleptest.pl/"
+});
